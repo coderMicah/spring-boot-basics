26878d4111080c0c5938c827eb10cab4487ca8f3
Micah <ismiki977@gmail.com>
2025-09-30 10:50:47 +0300

    Initial Commit

---
+++ src/main/java/com/mika/demo/Course.java
@@ -0,0 +1,31 @@
+package com.mika.demo;
+
+public class Course {
+    private long id;
+    private String name;
+    private String author;
+
+    public Course(long id, String name, String author) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    @Override
+    public String toString() {
+        return "Course[id= " + id + " name= " + name + " author= " + author;
+
+    }
+}
---
+++ src/main/java/com/mika/demo/CourseController.java
@@ -0,0 +1,21 @@
+package com.mika.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/courses")
+    public List<Course> getAll() {
+        return Arrays.asList(
+                new Course(1, "Learn AWS", "in28minutes"),
+                new Course(2, "Learn JS", "Brad Traversy"),
+                new Course(3, "Learn Laravel", "Adam Wathan"),
+                new Course(4, "Learn Typescript", "Matt Poccok"));
+    }
+
+}
